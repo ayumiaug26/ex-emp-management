895d25967b0a1a733e8f3da6efe44bb98013c003
ayumitanaka <ayumi.tanaka@rakus-partners.co.jp>
2024-02-08 17:57:15 +0900

    EmployeeControllerの追加 - 従業員一覧を表示するshowList()メソッドを実装.

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.Employee;
+import com.example.service.EmployeeService;
+
+@Controller
+@RequestMapping({ "/employee", "/employee/" })
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 従業員一覧を出力する.
+     * 
+     * @param model
+     * @return 従業員リスト
+     */
+    @GetMapping("/showList")
+    public String showList(Model model) {
+        List<Employee> employeeList = employeeService.showList();
+        model.addAttribute("employeeList", employeeList);
+        return "employee/list";
+    }
+
+}
